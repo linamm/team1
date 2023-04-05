de263da70c1bdb8aed7a364d5fbf5599c966a13e
hugo.cowan <hugo.cowan@vodafone.com>
2023-04-05 13:20:32 +0100

    Added carbon intensity API data

--- CardOne.js
+++ CardOne.js
@@ -2,13 +2,24 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const CardOne = () => {
+const CardOne = ({ carbonIntensityData }) => {
+
+  
+  if (carbonIntensityData.length === 0) {
     return (
       <View>
-        <Text style={{ fontSize: 18, fontWeight: 'bold' }}> Card One </Text>
-        <Text style={{ fontSize: 14 }}> Card One Description </Text>
+        Loading Carbon Intensity Data...
       </View>
-    );
+    )
+  }
+
+  return (
+    <View>
+      <Text style={{ fontSize: 18, fontWeight: 'bold' }}> Card One </Text>
+      <Text style={{ fontSize: 14 }}> Card One Description </Text>
+      <Text style={{ fontSize: 14 }}>intensity for the next half hour: {carbonIntensityData[0].intensity.index}</Text>
+    </View>
+  );
   };
   
   export default CardOne;
